0400b4835f6c0fa8606d0d848f15109667f44d30
Nautilus <nautilus@NautilusLnx.(none)>
2015-10-28 18:52:29 -0500

    Screenshot con ffmpeg 1 frme extract sh

--- InterfazControl/rtspShot.sh
+++ InterfazControl/rtspShot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TMP=tmp
+TMP="tmp.png"
 TMP_VID=$1/vid.mp4
 openRTSP -4 -f 30 -w 1280 -h 720 -b 300000 -d 0.1 -V rtsp://admin:12345@10.5.5.110:554  > $TMP_VID
-mplayer $TMP_VID -vo png:z=9:prefix=$TMP -frames 1 -msglevel all=-1
-mv $TMP'00000001'.png "$1/$2"
+ffmpeg -i $TMP_VID  -t 1 -f image2 $TMP 
+mv $TMP $1/$2
 rm $TMP_VID
