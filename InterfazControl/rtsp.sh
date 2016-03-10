fbaea7a50c156c9c47e8cf6b2624e1de0a7d9024
NautilusLnx <info@nautilusrobotics.com>
2016-03-10 18:06:07 -0500

    rtsp absolute path to destination file

--- InterfazControl/rtsp.sh
+++ InterfazControl/rtsp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openRTSP -4 -f 30 -w 1280 -h 720 -b 300000 -V rtsp://admin:12345@10.5.5.110:554  > ./Missions/$1/vid_$2.mp4
+openRTSP -4 -f 30 -w 1280 -h 720 -b 300000 -V rtsp://admin:12345@10.5.5.110:554  > /mnt/disk1/NautilusOne/Missions/$1/vid_$2.mp4
